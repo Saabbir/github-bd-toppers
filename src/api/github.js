1acb9b2a5d2096ae2c3f67c3a030c26972125cab
Saabbir Hossain <thesaabbir@gmail.com>
2023-01-26 15:39:39 +0600

    Read token from env variable

--- src/api/github.js
+++ src/api/github.js
@@ -4,7 +4,7 @@ export default axios.create({
   baseURL: "https://api.github.com",
   headers: {
     Accept: "application/vnd.github+json",
-    Authorization: "Bearer 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",
+    Authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
     //"X-GitHub-Api-Version": "2022-11-28",
   },
 });
