ec3172b7c92a24e34f02af466fb7bc2936ef6cb1
Saabbir Hossain <thesaabbir@gmail.com>
2023-01-26 15:11:35 +0600

    Update token

--- src/api/github.js
+++ src/api/github.js
@@ -4,7 +4,7 @@ export default axios.create({
   baseURL: "https://api.github.com",
   headers: {
     Accept: "application/vnd.github+json",
-    Authorization: "Bearer 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",
+    Authorization: "Bearer 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",
     //"X-GitHub-Api-Version": "2022-11-28",
   },
 });
