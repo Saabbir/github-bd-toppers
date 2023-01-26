50e2ef8d101e026103b43354328fbbee946bfb95
Saabbir Hossain <thesaabbir@gmail.com>
2023-01-26 21:03:41 +0600

    Remove Authorization header

--- src/api/github.js
+++ src/api/github.js
@@ -1,14 +1,10 @@
 import axios from "axios";
-console.log("saabbir: process.env.NODE_ENV", process.env.NODE_ENV);
-console.log(
-  "saabbir: process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN",
-  process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
-);
+
 export default axios.create({
   baseURL: "https://api.github.com",
   headers: {
     Accept: "application/vnd.github+json",
-    Authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+    // Authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
     //"X-GitHub-Api-Version": "2022-11-28",
   },
 });
